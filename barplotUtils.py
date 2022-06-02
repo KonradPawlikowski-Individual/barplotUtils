ae617a299c67b7ae07c271ba23a2811962d1f918
KonradPawlikowski-Individual <106261894+KonradPawlikowski-Individual@users.noreply.github.com>
2022-06-02 16:05:33 +0100

    Improved barplotUtils.multibar method
    - Improved bottom parameter compatibility for barplotUtils.stackbar
    - changed iwidth to igap and removed its multiplicative association for the intuitive linear interpretation in barplotUtils.multibar
    - added bgap, bar gap parameter to barplotUtils.multibar for intuitive customisation

--- barplotUtils.py
+++ barplotUtils.py
@@ -4,10 +4,10 @@ import pandas as pd
 
 supportedListTypes = (list,tuple, np.ndarray, pd.core.series.Series, pd.core.indexes.base.Index)
 
-def stackbar(axis, x, ys, stackOrder=None, kwargBroadcast={}, **kwargs):
+def stackbar(axis, x, ys, stackOrder=None, kwargBroadcast={}, bottom=0, **kwargs):
     '''
     Creates a stacked bar plot on the provided axis, x is the grouping,
-    ys must be of the same type and castable to a numpy array.
+    ys must be of the same type and castable to a NumPy array.
     kwargs are broadcast similar to how the method works in numpy, this can be
     toggled for individual kwargs by including them in the kwargBroadcast set.
     If no stack order is given the given order of elements will be used, if given
@@ -40,15 +40,23 @@ def stackbar(axis, x, ys, stackOrder=None, kwargBroadcast={}, **kwargs):
                 wideKwargs.add(key)
         else:
             thinKwargs[key] = kwargs[key]
+ 
+    yi = np.zeros(ys.shape[0])
     
-    # iterpret plot bottom
-    if 'bottom' in kwargs.keys():
-        yi = np.array(kwargs['bottom'])
+    # interpret bottom param
+    bottom = np.array(bottom)
+    if len(bottom.shape)<2:
+        yi += bottom
+        flag_bottom = False
     else:
-        yi = np.zeros(ys.shape[0])
+        flag_bottom = True
     
     # begin plotting
     for i in order:
+        # handle bottom param
+        if flag_bottom:
+            yi += bottom[i]
+        
         # find the kwargs for this plot
         thisKwargs = thinKwargs.copy()
         for key in wideKwargs:
@@ -59,40 +67,50 @@ def stackbar(axis, x, ys, stackOrder=None, kwargBroadcast={}, **kwargs):
     # end for
 # end method
 
-def multibar(axis, x, ys, iwidth=1, kwargBroadcast = {}, *args, **kwargs):
+def multibar(axis, x, ys, igap=0, bgap=0, kwargBroadcast={}, **kwargs):
     '''
-    Creates a multiple bar plot on the provided axis, x is the grouping, ys must be of the same type and
-    castable to a pandas dataframe (e.g a 2d array) or a pandas dataframe.
-    args are strictly passed, as is, to the plot method while kwargs are broadcast by default, this
-    can be toggled for individual kwargs by including them in the kwargBroadcast
-    set. iwidth creates a 'gap' between sets of data multiplicitively, iwidth should be a value >0 but <=1.
+    Creates a multiple bar plot on the provided axis, x is the grouping, ys must be castable to a NumPy array.
+    Kwargs are broadcast by default, this can be toggled for individual kwargs by including them in the kwargBroadcast
+    set. igap creates an intermediate gap between multiple bars while bgap creates a gap between the bars, both should be
+    given as ratios (since the multibar placements are standardised to integer indicies).
     '''
     
     # cast all data to numpy array format
     x = np.array(x)
     ys = np.array(ys)
     
-    # initialise the width
-    width = iwidth/ys.shape[1]
+    # define default width
+    offset = np.zeros(ys.shape[1])
+    if 'width' not in kwargs.keys():
+        kwargs['width'] = (1-igap-bgap)/ys.shape[1]
+        offset = np.arange(ys.shape[1])*(kwargs['width']+bgap/ys.shape[1])
     
     # for labelling xticks
     xlabel = x[::]
     x = np.arange(0,len(x))
     
+    # group the kwargs
+    thinKwargs = {}
+    wideKwargs = set()
+    for key in kwargs.keys():
+        if key in kwargBroadcast:
+            thinKwargs[key] = kwargs[key]
+        elif isinstance(kwargs[key],supportedListTypes):
+            for i in range(ys.shape[1]):
+                wideKwargs.add(key)
+        else:
+            thinKwargs[key] = kwargs[key]
+    
     # begin plotting
     for i in range(ys.shape[1]):
         # find the kwargs for this plot
-        thisKwargs = {}
-        for key in kwargs.keys():
-            if key in kwargBroadcast:
-                thisKwargs[key] = kwargs[key]
-            elif isinstance(kwargs[key],supportedListTypes):
-                thisKwargs[key] = kwargs[key][i]
-            else:
-                thisKwargs[key] = kwargs[key]
+        thisKwargs = thinKwargs.copy()
+        for key in wideKwargs:
+            thisKwargs[key] = kwargs[key][i]
         # end for
-        axis.bar(x+i*width, ys[::,i], width=width, *args, **thisKwargs)
+            
+        axis.bar(x+offset[i], ys[::,i], **thisKwargs)
     # end for
-    axis.set_xticks(x+(i*width)/2)
+    axis.set_xticks(x+offset[i]/2)
     axis.set_xticklabels(xlabel)
 # end method
